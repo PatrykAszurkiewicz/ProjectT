d968f48b782e6ec02b90c85d070325d8f340218b
ActiveP <patrykos65a@gmail.com>
2025-08-10 14:44:54 +0200

    Basic Menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
