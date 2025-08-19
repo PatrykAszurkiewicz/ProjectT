e82c7b083442decbdbb974da63139c1249c96438
ActiveP <patrykos65a@gmail.com>
2025-08-19 20:49:31 +0200

    Updated scenes

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("StartingWeapon");
     }
     public void OpenOptions()
     {
